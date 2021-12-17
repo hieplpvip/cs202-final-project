8d6229fa73d9b27b63b96fce55c666ca198bcd7c
dvtngan <dvtngan@gmail.com>
2021-12-17 15:40:36 +0700

    fix stupid bugs

--- src/Bird.cpp
+++ src/Bird.cpp
@@ -4,7 +4,7 @@ void Bird::draw(olc::PixelGameEngine* pge) {
 	if (direction == LEFT)
 		sprite = new olc::Sprite("assets/graphics/Bird_fly_left.png");
 	//sprite = new olc::Sprite("assets/graphics/Bird_walk_left.png");
-	else sprite == new olc::Sprite("assets/graphics/Bird_fly_right.png");
+	else sprite = new olc::Sprite("assets/graphics/Bird_fly_right.png");
 	//sprite = new olc::Sprite("assets/graphics/Bird_fly_left.png");
 	Obstacle::draw(pge);
 }
--- src/Car.cpp
+++ src/Car.cpp
@@ -3,7 +3,7 @@
 void Car::draw(olc::PixelGameEngine* pge) {
 	if (direction == LEFT)
 		sprite = new olc::Sprite("assets/graphics/Car_left.png");
-	else sprite == new olc::Sprite("assets/graphics/Car_right.png");
+	else sprite = new olc::Sprite("assets/graphics/Car_right.png");
 	Obstacle::draw(pge);
 }
 
