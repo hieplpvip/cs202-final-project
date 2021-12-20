4f2735e1c58ebd18594266bcf012f92e47af9ef9
dinhmyky <dinhmyky02@gmail.com>
2021-12-20 09:37:49 +0700

    Update Coin.cpp

--- src/Coin.cpp
+++ src/Coin.cpp
@@ -1,7 +1,7 @@
 #include "Coin.h"
 
 void Coin::draw(olc::PixelGameEngine* pge) {
-	sprite = new olc::Sprite("assets/graphics/Coin.png");
+	sprite = new olc::Sprite("assets/graphics/Coin S.png");
 	if (sprite)
 		pge->DrawSprite(pos, sprite);
 }
