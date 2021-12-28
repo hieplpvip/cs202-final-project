f417e9a7aafae7078349b33dff83f7dbe0e55882
Le Bao Hiep <baohiep2013@gmail.com>
2021-12-28 20:09:52 +0700

    Win screen

--- src/Constants.h
+++ src/Constants.h
@@ -8,6 +8,9 @@ namespace Constants {
   const char* TITLE = "Crossing Road";
   const float TITLE_DURATION = 3.0f;
 
+  const char* WIN = "You win!";
+  const float WIN_DURATION = 3.0f;
+
   const int NUMBER_OF_LEVELS = 10;
 
   std::vector<std::string> MENU_ITEMS = {"New Game", "Load Game", "Settings", "Exit"};
